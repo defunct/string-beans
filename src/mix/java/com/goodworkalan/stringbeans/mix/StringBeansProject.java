52c309976e6d7996661813f0607287606e928a98
Alan Gutierrez <alan@blogometer.com>
2010-03-14 15:59:52 -0500

    Removed Comfort I/O as a dependency.

--- src/mix/java/com/goodworkalan/stringbeans/mix/StringBeansProject.java
+++ src/mix/java/com/goodworkalan/stringbeans/mix/StringBeansProject.java
@@ -14,7 +14,6 @@ public class StringBeansProject extends ProjectModule {
                 .main()
                     .depends()
                         .artifact(new Artifact("com.github.bigeasy.danger/danger/0.1"))
-                        .artifact(new Artifact("com.goodworkalan/comfort-io/0.1.1"))
                         .artifact(new Artifact("com.goodworkalan/reflective/0.1"))
                         .end()
                     .end()
