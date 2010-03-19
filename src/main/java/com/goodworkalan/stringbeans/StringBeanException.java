eae485c40748da1a6da84238ee570a310dca6d56
Alan Gutierrez <alan@blogometer.com>
2010-03-18 21:33:24 -0500

    Added positioned arguments to StringBeanException.

--- src/main/java/com/goodworkalan/stringbeans/StringBeanException.java
+++ src/main/java/com/goodworkalan/stringbeans/StringBeanException.java
@@ -10,11 +10,11 @@ public class StringBeanException extends ContextualDanger {
     /** A cache of resource bundles. */
     private final static ConcurrentHashMap<String, ResourceBundle> bundles = new ConcurrentHashMap<String, ResourceBundle>();
 
-    public StringBeanException(Class<?> context, String code) {
-        super(bundles, context, code, null);
+    public StringBeanException(Class<?> context, String code, Object...arguments) {
+        super(bundles, context, code, null, arguments);
     }
     
     public StringBeanException(Class<?> context, String code, Throwable cause, Object...arguments) {
-        super(bundles, context, code, cause);
+        super(bundles, context, code, cause, arguments);
     }
 }
