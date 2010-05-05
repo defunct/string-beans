578e24a7018983ee03f6a1ad46f3b729eed4791d
Alan Gutierrez <alan@blogometer.com>
2010-05-05 01:02:31 -0500

    Upated Mix build for new project language.

--- src/mix/java/com/goodworkalan/stringbeans/jpa/mix/StringBeansJpaProject.java
+++ src/mix/java/com/goodworkalan/stringbeans/jpa/mix/StringBeansJpaProject.java
@@ -1,6 +1,5 @@
 package com.goodworkalan.stringbeans.jpa.mix;
 
-import com.goodworkalan.go.go.Artifact;
 import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
@@ -10,18 +9,18 @@ public class StringBeansJpaProject extends ProjectModule {
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
-                .produces(new Artifact("com.github.bigeasy.string-beans/string-beans-jpa/0.1"))
+                .produces("com.github.bigeasy.string-beans/string-beans-jpa/0.1")
                 .main()
                     .depends()
-                        .artifact(new Artifact("com.goodworkalan/string-beans/0.1"))
-                        .artifact(new Artifact("org.hibernate/hibernate-core/3.3.1.GA"))
-                        .artifact(new Artifact("org.hibernate/hibernate-annotations/3.4.0.GA"))
+                        .include("com.goodworkalan/string-beans/0.1")
+                        .include("org.hibernate/hibernate-core/3.3.1.GA")
+                        .include("org.hibernate/hibernate-annotations/3.4.0.GA")
                         .end()
                     .end()
                 .test()
                     .depends()
-                        .artifact(new Artifact("org.testng/testng/5.10/jdk15"))
-                        .artifact(new Artifact("org.mockito/mockito-core/1.6"))
+                        .include("org.testng/testng-jdk15/5.10")
+                        .include("org.mockito/mockito-core/1.6")
                         .end()
                     .end()
                 .end()
