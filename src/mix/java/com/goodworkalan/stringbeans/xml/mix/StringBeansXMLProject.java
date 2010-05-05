a661ad1f9f0c3a0bec877f76ef00e7a448377a55
Alan Gutierrez <alan@blogometer.com>
2010-05-04 23:32:54 -0500

    Updated Mix build for new project language.

--- src/mix/java/com/goodworkalan/stringbeans/xml/mix/StringBeansXMLProject.java
+++ src/mix/java/com/goodworkalan/stringbeans/xml/mix/StringBeansXMLProject.java
@@ -1,6 +1,5 @@
 package com.goodworkalan.stringbeans.mix;
 
-import com.goodworkalan.go.go.Artifact;
 import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
@@ -10,16 +9,16 @@ public class StringBeansXMLProject extends ProjectModule {
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
-                .produces(new Artifact("com.goodworkalan/string-beans-xml/0.1"))
+                .produces("com.goodworkalan/string-beans-xml/0.1")
                 .main()
                     .depends()
-                        .artifact(new Artifact("com.goodworkalan/string-beans/0.1"))
+                        .include("com.goodworkalan/string-beans/0.+1")
                         .end()
                     .end()
                 .test()
                     .depends()
-                        .artifact(new Artifact("org.testng/testng/5.10/jdk15"))
-                        .artifact(new Artifact("org.mockito/mockito-core/1.6"))
+                        .include("org.testng/testng-jdk15/5.10")
+                        .include("org.mockito/mockito-core/1.6")
                         .end()
                     .end()
                 .end()
