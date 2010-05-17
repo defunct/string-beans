10449e1447bab219caa22642a99fc16d724bce6a
Alan Gutierrez <alan@blogometer.com>
2010-05-16 23:09:46 -0500

    Mix ProjectModule is now an interface.

--- src/mix/java/com/goodworkalan/stringbeans/jpa/mix/StringBeansJpaProject.java
+++ src/mix/java/com/goodworkalan/stringbeans/jpa/mix/StringBeansJpaProject.java
@@ -4,24 +4,28 @@ import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
 
-public class StringBeansJpaProject extends ProjectModule {
-    @Override
+/**
+ * Builds the project definition for String Beans JPA.
+ *
+ * @author Alan Gutierrez
+ */
+public class StringBeansJpaProject implements ProjectModule {
+    /**
+     * Build the project definition for String Beans JPA.
+     *
+     * @param builder
+     *          The project builder.
+     */
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
                 .produces("com.github.bigeasy.string-beans/string-beans-jpa/0.1")
-                .main()
-                    .depends()
-                        .include("com.github.bigeasy.string-beans/string-beans/0.+1")
-                        .include("org.hibernate/hibernate-core/3.3.1.GA")
-                        .include("org.hibernate/hibernate-annotations/3.4.0.GA")
-                        .end()
-                    .end()
-                .test()
-                    .depends()
-                        .include("org.testng/testng-jdk15/5.10")
-                        .include("org.mockito/mockito-core/1.6")
-                        .end()
+                .depends()
+                    .production("com.github.bigeasy.string-beans/string-beans/0.+1")
+                    .production("org.hibernate/hibernate-core/3.3.1.GA")
+                    .production("org.hibernate/hibernate-annotations/3.4.0.GA")
+                    .development("org.testng/testng-jdk15/5.10")
+                    .development("org.mockito/mockito-core/1.6")
                     .end()
                 .end()
             .end();
