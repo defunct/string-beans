c2137786a7381ac32bb6624695db8b6605749e15
Alan Gutierrez <alan@blogometer.com>
2010-05-20 13:03:42 -0500

    Version bump.

--- src/mix/java/com/goodworkalan/stringbeans/mix/StringBeansProject.java
+++ src/mix/java/com/goodworkalan/stringbeans/mix/StringBeansProject.java
@@ -19,7 +19,7 @@ public class StringBeansProject implements ProjectModule {
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
-                .produces("com.github.bigeasy.string-beans/string-beans/0.1.0.1")
+                .produces("com.github.bigeasy.string-beans/string-beans/0.1.0.2")
                 .depends()
                     .production("com.github.bigeasy.stash/stash/0.+1")
                     .production("com.github.bigeasy.danger/danger/0.+1")
