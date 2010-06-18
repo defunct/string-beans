c5bc6a732a0c5f489e14a31ae5d4a52ea50841bf
Alan Gutierrez <alan@blogometer.com>
2010-06-17 19:17:54 -0500

    Changed class-boxer/association artifact strings.

--- src/mix/java/com/goodworkalan/stringbeans/mix/StringBeansProject.java
+++ src/mix/java/com/goodworkalan/stringbeans/mix/StringBeansProject.java
@@ -19,13 +19,13 @@ public class StringBeansProject implements ProjectModule {
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
-                .produces("com.github.bigeasy.string-beans/string-beans/0.1.0.3")
+                .produces("com.github.bigeasy.string-beans/string-beans/0.1.0.4")
                 .depends()
                     .production("com.github.bigeasy.stash/stash/0.+1")
-                    .production("com.github.bigeasy.class-boxer/class-boxer/0.+1")
+                    .production("com.github.bigeasy.class/class-boxer/0.+1")
                     .production("com.github.bigeasy.infuse/infuse/0.+1")
                     .production("com.github.bigeasy.danger/danger/0.+1")
-                    .production("com.github.bigeasy.class-association/class-association/0.+1")
+                    .production("com.github.bigeasy.class/class-association/0.+1")
                     .production("com.github.bigeasy.diffuse/diffuse/0.+1")
                     .production("com.github.bigeasy.reflective/reflective/0.+1")
                     .development("org.testng/testng-jdk15/5.10")
