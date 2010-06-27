07f0c00a57cfb46ad096d15977f8cd50eefd614d
Alan Gutierrez <alan@blogometer.com>
2010-06-26 21:22:42 -0500

    Added Javadoc TODOs.

--- src/main/java/com/goodworkalan/stringbeans/url/StringToObjectMap.java
+++ src/main/java/com/goodworkalan/stringbeans/url/StringToObjectMap.java
@@ -2,6 +2,7 @@ package com.goodworkalan.stringbeans.url;
 
 import java.util.HashMap;
 
+// TODO Document.
 @SuppressWarnings("serial")
 class StringToObjectMap extends HashMap<String, Object> {
 }
--- src/main/java/com/goodworkalan/stringbeans/url/UrlParser.java
+++ src/main/java/com/goodworkalan/stringbeans/url/UrlParser.java
@@ -10,13 +10,17 @@ import com.goodworkalan.stringbeans.CollectionParser;
 import com.goodworkalan.stringbeans.StringBeanException;
 import com.goodworkalan.stringbeans.Converter;
 
+// TODO Document.
 public class UrlParser {
+    // TODO Document.
     private final CollectionParser collectionParser;
     
+    // TODO Document.
     public UrlParser(Converter converter) {
         this.collectionParser = new CollectionParser(converter, true);
     }
     
+    // TODO Document.
     private StringToObjectMap tree(Map<String, String> form) {
         StringToObjectMap tree = new StringToObjectMap(); 
         for (Map.Entry<String, String> entry : form.entrySet()) {
@@ -28,19 +32,23 @@ public class UrlParser {
         }
         return tree;
     }
-    
+
+    // TODO Document.
     public Stash getStash() {
         return collectionParser.getStash();
     }
     
+    // TODO Document.
     public <T> void populate(T rootObject, Map<String, String> form) {
         collectionParser.populate(rootObject, tree(form));
     }
     
+    // TODO Document.
     public <T> T create(Class<T> rootClass, Map<String, String> form) {
         return collectionParser.create(rootClass, tree(form));
     }
 
+    // TODO Document.
     private static boolean set(Path path, Object container, Object value, int index) {
         Part part = path.get(index);
         return part.isInteger()
@@ -48,10 +56,12 @@ public class UrlParser {
              : put(path, (StringToObjectMap) container, part.getName(), value, index);
     }
     
+    // TODO Document.
     private static Object newContainer(Path path, int partIndex) {
         return path.get(partIndex).isInteger() ? new SparseCollection() : new StringToObjectMap();
     }
     
+    // TODO Document.
     private static void assertContainerType(Path path, int partIndex, Object container) {
         Class<?> containerClass = path.get(partIndex).isInteger() ? SparseCollection.class : StringToObjectMap.class;
         if (!containerClass.isAssignableFrom(container.getClass())) {
@@ -59,6 +69,7 @@ public class UrlParser {
         }
     }
     
+    // TODO Document.
     private static <K> boolean put(Path path, Map<K, Object> map, K key, Object value, int partIndex) {
         Part part = path.get(partIndex);
         if (partIndex == path.size() - 1) {
