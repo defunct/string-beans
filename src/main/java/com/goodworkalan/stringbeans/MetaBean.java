873385c74f4e37954be757596dfef1df8967d9c2
Alan Gutierrez <alan@blogometer.com>
2010-06-26 22:55:54 -0500

    MetaBean now uses Reflective Setter.

--- src/main/java/com/goodworkalan/stringbeans/MetaBean.java
+++ src/main/java/com/goodworkalan/stringbeans/MetaBean.java
@@ -1,64 +1,37 @@
 package com.goodworkalan.stringbeans;
 
-import java.beans.BeanInfo;
-import java.beans.IntrospectionException;
-import java.beans.Introspector;
-import java.beans.PropertyDescriptor;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
 import java.lang.reflect.Type;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 import com.goodworkalan.reflective.Reflective;
 import com.goodworkalan.reflective.ReflectiveException;
+import com.goodworkalan.reflective.setter.Setters;
 
-// TODO Document.
+/**
+ * The basic serialization operations and properties for a Java Bean.
+ * 
+ * @author Alan Gutierrez
+ */
 public class MetaBean implements BeanConstructor {
-    // TODO Document.
-    private final Type type;
+    /** The bean class. */
     private final Class<?> objectClass;
-    private final Set<String> names = new HashSet<String>();
-    
-    private final Map<String, Method> writers = new HashMap<String, Method>();
 
-    private final Map<String, Method> readers = new HashMap<String, Method>();
-    private final Map<String, Field> fields = new HashMap<String, Field>();
-
+    /**
+     * Create a meta bean for the given bean class.
+     * 
+     * @param objectClass
+     *            The bean class.
+     */
     public MetaBean(Class<?> objectClass) {
-        BeanInfo beanInfo;
-        try {
-            beanInfo = Introspector.getBeanInfo(objectClass);
-        } catch (IntrospectionException e) {
-            throw new StringBeanException(MetaBean.class, "getBeanInfo", e);
-        }
-        for (PropertyDescriptor property : beanInfo.getPropertyDescriptors()) {
-            java.lang.reflect.Method reader = property.getReadMethod();
-            java.lang.reflect.Method writer = property.getWriteMethod();
-            if (!(reader == null && writer == null)) {
-                if (writer != null) {
-                    writers.put(property.getName(), writer);
-                }
-                if (reader != null) {
-                    names.add(property.getName());
-                    readers.put(property.getName(), reader);
-                }
-            }
-        }
-        for (java.lang.reflect.Field field : objectClass.getFields()) {
-            if (Modifier.isPublic(field.getModifiers()) && !names.contains(field.getName())) {
-                names.add(field.getName());
-                fields.put(field.getName(), field);
-            }
-        }
-        this.type = objectClass;
         this.objectClass = objectClass;
     }
 
+    /**
+     * Create a new instance of the bean to place onto the stack to collect
+     * property values.
+     * 
+     * @return A new bean instance.
+     */
     public Object newStackInstance() {
         try {
             try {
@@ -71,76 +44,40 @@ public class MetaBean implements BeanConstructor {
         }
     }
 
+    /**
+     * Get the bean class.
+     * 
+     * @return The bean class.
+     */
     public Class<?> getObjectClass() {
         return objectClass;
     }
 
-    public Type getType() {
-        return type;
-    }
-
-    public Set<String> getPropertyNames() {
-        return Collections.unmodifiableSet(names);
-    }
-
-    public Class<?> getPropertyClass(String name) {
-        Field field = fields.get(name);
-        if (field == null) {
-            Method reader = readers.get(name);
-            return reader.getReturnType();
-        }
-        return field.getType();
-    }
-
+    /**
+     * Get the type of the given property.
+     * 
+     * @return The property type.
+     */
     public Type getPropertyType(String name) {
-        Field field = fields.get(name);
-        if (field == null) {
-            Method reader = readers.get(name);
-            if (reader == null) {
-                Method writer = writers.get(name);
-                if (writer == null) {
-                    return null;
-                }
-                return writer.getGenericParameterTypes()[0];
-            }
-            return reader.getGenericReturnType();
-        }
-        return field.getGenericType();
+        return Setters.getGetters(objectClass).get(name).getGenericType();
     }
 
+    /**
+     * Set the property of the given object with the given name to the given
+     * value.
+     * 
+     * @param object
+     *            The object.
+     * @param name
+     *            The name.
+     * @param value
+     *            The value.
+     */
     public void set(Object object, String name, Object value) {
-     try {
-            try {
-                Field field = fields.get(name);
-                if (field == null) {
-                    Method writer = writers.get(name);
-                    if (writer != null) {
-                        writer.invoke(object, value);
-                    }
-                } else {
-                    field.set(object, value);
-                }
-            } catch (Throwable e) {
-                throw new ReflectiveException(Reflective.encode(e), e);
-            }
+        try {
+            Setters.getGetters(objectClass).get(name).set(object, value);
         } catch (ReflectiveException e) {
             throw new StringBeanException(MetaBean.class, "fieldSet", e);
-        }
-    }
-
-    public Object get(Object object, String name) {
-        try {
-            try {
-                Field field = fields.get(name);
-                if (field == null) {
-                    return readers.get(name).invoke(object);
-                }
-                return field.get(object);
-            } catch (Throwable e) {
-                throw new ReflectiveException(Reflective.encode(e), e);
-            }
-        } catch (ReflectiveException e) {
-            throw new StringBeanException(MetaBean.class, "fieldGet", e);
         }
     }
 
