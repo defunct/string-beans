c119691365e91bdc7777ebf16640e67a84bacb9e
Alan Gutierrez <alan@blogometer.com>
2010-07-12 11:12:44 -0500

    Removed dependency on Danger and Reflective.

--- src/mix/java/com/goodworkalan/stringbeans/url/mix/StringBeansUrlProject.java
+++ src/mix/java/com/goodworkalan/stringbeans/url/mix/StringBeansUrlProject.java
@@ -19,14 +19,12 @@ public class StringBeansUrlProject implements ProjectModule {
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
-                .produces("com.github.bigeasy.string-beans/string-beans-url/0.1")
+                .produces("com.github.bigeasy.string-beans/string-beans-url/0.1.0.1")
                 .depends()
                     .production("com.github.bigeasy.string-beans/string-beans/0.+1")
                     .production("com.github.bigeasy.infuse/infuse/0.+1")
                     .production("com.github.bigeasy.permeate/permeate/0.+1")
-                    .production("com.github.bigeasy.danger/danger/0.+1")
                     .production("com.github.bigeasy.diffuse/diffuse/0.+1")
-                    .production("com.github.bigeasy.reflective/reflective/0.+1")
                     .development("org.testng/testng-jdk15/5.10")
                     .development("org.mockito/mockito-core/1.6")
                     .end()
